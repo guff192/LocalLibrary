5bd52442e74c2aea423bc05e984fc5204814a8a6
guff192 <kochkons1@ya.ru>
2021-04-28 15:27:24 +0300

    Added adaptive menu for mobile devices, favicon. Added visits counter in session vars

---
+++ catalog/static/js/script.js
@@ -0,0 +1,16 @@
+function showMenu() {
+    let sidebar_nav = document.getElementsByClassName("sidebar-nav").item(0);
+    let side_menu = document.getElementsByClassName("sidebar").item(0);
+
+    if (sidebar_nav.classList.contains("hidden")) {
+        sidebar_nav.classList.remove("hidden");
+        side_menu.classList.remove("tight");
+    } else {
+        sidebar_nav.classList.add("hidden");
+        side_menu.classList.add("tight");
+    }
+
+}
+
+let open_menu_button = document.getElementById("open-menu-button");
+open_menu_button.onclick = showMenu;
